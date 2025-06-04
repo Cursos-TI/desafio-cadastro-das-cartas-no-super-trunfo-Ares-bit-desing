2aa8f5688d74f677304e7fe77ca4ce596b9fccd8
Ares-bit-desing <anakarolinar12@gmail.com>
2025-06-04 00:38:28 +0000

    adicionei um printf e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,11 +18,13 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    int populaçao, numerodepontosturisticos;
+    int populaçao, nturistico;
     char estado;
     char codigodecarta[5], nomedacidade[15];
     float areadekm2, PIB;
 
+    printf("Digite o nome do Estado: \n");
+    scanf("%c", &estado);
 
 
     return 0;
